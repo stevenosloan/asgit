83bfcdab4d60ccbe36808a040dad2445fba040a5
Steven Sloan <stevenosloan@gmail.com>
2013-11-01 00:12:53 -0400

    add missing spec for Url::project

--- spec/asgit/url_spec.rb
+++ spec/asgit/url_spec.rb
@@ -14,6 +14,12 @@ describe Asgit::Url do
     Asgit::Services.send(:remove_instance_variable, "@_github")
   end
 
+  describe "::project" do
+    it "returns correct url for the project" do
+      expect( Asgit::Url.project ).to eq "https://github.com/wu/tang"
+    end
+  end
+
   describe "::commit" do
     it "returns the correct url for the provided commit" do
       expect( Asgit::Url.commit "woot" ).to eq "https://github.com/wu/tang/commit/woot"
