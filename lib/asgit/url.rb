5e729af52ad197341fccadbbf465ee1b08f6abdd
Steven Sloan <stevenosloan@gmail.com>
2013-12-09 01:08:18 -0500

    rewrite Asgit::Url to be a class vs module, details & service are passed as dependencies

--- lib/asgit/url.rb
+++ lib/asgit/url.rb
@@ -1,50 +1,53 @@
 module Asgit
-  module Url
+  class Url
 
-    class << self
+    attr_reader :details, :service
 
-      def project
-        Asgit.config.service.base_structure % {
-          base_url: Asgit.config.service.base_url,
-          organization: Asgit.config.organization,
-          project: Asgit.config.project
-        }
-      end
-
-      def commit sha
-        File.join( project, Asgit.config.service.commit_uri % { commit: sha } )
-      end
-
-      def branch name
-        File.join( project, Asgit.config.service.branch_uri % { branch: name } )
-      end
-
-      def file file_path, options={}
-        file_path = file_path.gsub( /^\//, '' )
-        branch    = options.fetch(:branch) { Asgit.config.default_branch }
-        line      = options.has_key?(:line) ? format_lines(options[:line]) : ''
-
-        File.join( project, Asgit.config.service.file_uri % { file_path: file_path, branch: branch, line: line } )
-      end
-
-      def file_at_commit file_path, commit=commit, options={}
-        file_path = file_path.gsub( /^\//, '' )
-        line      = options.has_key?(:line) ? format_lines(options[:line]) : ''
-
-        File.join( project, Asgit.config.service.file_at_commit_uri % { file_path: file_path, commit: commit, line: line } )
-      end
-
-      private
-
-        def format_lines input
-          if input.respond_to?(:begin) && input.respond_to?(:end)
-            return "#L#{input.begin}-L#{input.end}"
-          else
-            return "#L#{input}"
-          end
-        end
-
+    def initialize details, service
+      @details = details
+      @service = service
     end
+
+    def project
+      service.base_structure % {
+        base_url: service.base_url,
+        organization: details.organization,
+        project: details.project
+      }
+    end
+
+    def commit sha
+      File.join( project, service.commit_uri % { commit: sha } )
+    end
+
+    def branch name
+      File.join( project, service.branch_uri % { branch: name } )
+    end
+
+    def file file_path, options={}
+      file_path = file_path.gsub( /^\//, '' )
+      branch    = options.fetch(:branch) { details.default_branch }
+      line      = options.has_key?(:line) ? format_lines(options[:line]) : ''
+
+      File.join( project, service.file_uri % { file_path: file_path, branch: branch, line: line } )
+    end
+
+    def file_at_commit file_path, commit=commit, options={}
+      file_path = file_path.gsub( /^\//, '' )
+      line      = options.has_key?(:line) ? format_lines(options[:line]) : ''
+
+      File.join( project, service.file_at_commit_uri % { file_path: file_path, commit: commit, line: line } )
+    end
+
+    private
+
+      def format_lines input
+        if input.respond_to?(:begin) && input.respond_to?(:end)
+          return "#L#{input.begin}-L#{input.end}"
+        else
+          return "#L#{input}"
+        end
+      end
 
   end
 end
---
+++ spec/lib/asgit/url_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+describe Asgit::Url do
+
+  let(:service) { Asgit::Services.github }
+  let(:details) do
+    instance_double( "Asgit::Project::Details",
+      organization: 'stevenosloan',
+      project: 'asgit',
+      default_branch: 'master'
+    )
+  end
+
+  let(:subject) { described_class.new( details, service ) }
+
+  describe "#initialize" do
+    it "sets the details & service attributes" do
+      expect( subject.details ).to eq details
+      expect( subject.service ).to eq service
+    end
+  end
+
+  describe "#project" do
+    it "returns expected url" do
+      expect( subject.project ).to eq 'https://github.com/stevenosloan/asgit'
+    end
+  end
+
+  describe "#commit" do
+    it "returns url of commit" do
+      expect( subject.commit 'commit_sha' ).to eq 'https://github.com/stevenosloan/asgit/commit/commit_sha'
+    end
+  end
+
+  describe "#branch" do
+    it "returns url of the branch" do
+      expect( subject.branch 'branch_name' ).to eq 'https://github.com/stevenosloan/asgit/tree/branch_name'
+    end
+  end
+
+  describe '#file' do
+    it "returns url for the file" do
+      expect( subject.file 'lib/asgit.rb' ).to eq 'https://github.com/stevenosloan/asgit/blob/master/lib/asgit.rb'
+    end
+    it "adjusts for paths with leading slash" do
+      expect( subject.file '/lib/asgit.rb' ).to eq 'https://github.com/stevenosloan/asgit/blob/master/lib/asgit.rb'
+    end
+    it "returns url for the file on a specific branch" do
+      expect( subject.file 'lib/asgit.rb', branch: 'branch_name' ).to eq 'https://github.com/stevenosloan/asgit/blob/branch_name/lib/asgit.rb'
+    end
+    it "returns url for the file with a line number" do
+      expect( subject.file 'lib/asgit.rb', line: '15' ).to eq 'https://github.com/stevenosloan/asgit/blob/master/lib/asgit.rb#L15'
+    end
+    it "returns url for the file with a range of line numbers" do
+      expect( subject.file 'lib/asgit.rb', line: (15..18) ).to eq 'https://github.com/stevenosloan/asgit/blob/master/lib/asgit.rb#L15-L18'
+    end
+  end
+
+  describe "#file_at_commit" do
+    it "returns url for file at commit_sha" do
+      expect( subject.file_at_commit 'lib/asgit.rb', 'commit_sha' ).to eq 'https://github.com/stevenosloan/asgit/blob/commit_sha/lib/asgit.rb'
+    end
+    it "returns url for file at commit_sha with line numbers" do
+      expect( subject.file_at_commit 'lib/asgit.rb', 'commit_sha', line: '15' ).to eq 'https://github.com/stevenosloan/asgit/blob/commit_sha/lib/asgit.rb#L15'
+    end
+    it "returns url for file at commit_sha with range of line numbers" do
+      expect( subject.file_at_commit 'lib/asgit.rb', 'commit_sha', line: (15..18) ).to eq 'https://github.com/stevenosloan/asgit/blob/commit_sha/lib/asgit.rb#L15-L18'
+    end
+  end
+
+end
