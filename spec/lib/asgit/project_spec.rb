d1cd831b045ada9706633c308d87ed2ac2306bbd
Steven Sloan <stevenosloan@gmail.com>
2013-12-09 00:23:40 -0500

    spec the memoized details method

--- spec/lib/asgit/project_spec.rb
+++ spec/lib/asgit/project_spec.rb
@@ -2,20 +2,26 @@ require 'spec_helper'
 
 describe Asgit::Project do
 
+  let(:default_details) do
+    {
+      service: :github,
+      organization: 'stevenosloan',
+      project: 'asgit',
+      default_branch: 'master'
+    }
+  end
+
   describe "#initialize" do
     it "sets detail's attributes" do
       details_double = instance_double( "Asgit::Project::Details" )
       allow( Asgit::Project::Details ).to receive(:new).and_return(details_double)
 
-      expect( details_double ).to receive(:service=)
-      expect( details_double ).to receive(:organization=)
-      expect( details_double ).to receive(:project=)
-      expect( details_double ).to receive(:default_branch=)
+      expect( details_double ).to receive(:service=).with(:github)
+      expect( details_double ).to receive(:organization=).with('stevenosloan')
+      expect( details_double ).to receive(:project=).with('asgit')
+      expect( details_double ).to receive(:default_branch=).with('master')
 
-      described_class.new( service: :github,
-                           organization: 'stevenosloan',
-                           project: 'asgit',
-                           default_branch: 'master' )
+      described_class.new( default_details )
     end
 
     it "raises an ArgumentError if an out-of-scope arg is passed" do
@@ -26,6 +32,15 @@ describe Asgit::Project do
   end
 
   describe "#details" do
+    let(:project) { described_class.new( default_details ) }
+
+    it "returns a Details instance" do
+      expect( project.details.is_a? Asgit::Project::Details ).to be_truthy
+    end
+
+    it "returns the same instance on each call" do
+      expect( project.details ).to eq project.details
+    end
   end
 
 end
